d92a20bf62cc955c2e523a054d4fc7f544dc4a0d
Matheus Sousa <matheussousab2014@gmail.com>
2024-03-15 14:59:50 -0300

    add: rotas para as paginas de esqueceu senha

--- src/routes/Public/PublicRoutes.jsx
+++ src/routes/Public/PublicRoutes.jsx
@@ -5,9 +5,13 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import InicioUserPage from "../../pages/User/Auth/Inicio";
 import LoginUserPage from "../../pages/User/Auth/Login";
 import RegisterUserPage from "../../pages/User/Auth/Register";
+import ForgotPasswordUserPage from "../../pages/User/Auth/ForgotPassword";
+import ResetPasswordUserPage from "../../pages/User/Auth/ResetPassword";
 
 // PAGINAS ADMINISTRADOR
 import LoginAdminPage from "../../pages/Admin/Auth/Login";
+import ForgotPasswordAdminPage from "../../pages/Admin/Auth/ForgotPassword";
+import ResetPasswordAdminPage from "../../pages/Admin/Auth/ResetPassword";
 
 export default function PublicRoutes() {
     return (
@@ -16,8 +20,12 @@ export default function PublicRoutes() {
             <Route path="/" element={<InicioUserPage />} />
             <Route path="/login" element={<LoginUserPage />} />
             <Route path="/register" element={<RegisterUserPage />} />
+            <Route path="/forgotpassword" element={<ForgotPasswordUserPage />} />
+            <Route path="/resetpassword/:token" element={<ResetPasswordUserPage />} />
             {/* ROTAS PUBLICAS PARA ADMINISTRADOR */}
             <Route path="/admin" element={<LoginAdminPage />} />
+            <Route path="/admin/forgotpassword" element={<ForgotPasswordAdminPage />} />
+            <Route path="/admin/resetpassword/:token" element={<ResetPasswordAdminPage />} />
         </Routes>
     )
 } 
