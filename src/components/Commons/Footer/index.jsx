556a355fe8dd34b654b3916293ecc3a366ddf6af
Matheus Sousa <matheussousab2014@gmail.com>
2024-03-18 13:37:35 -0300

    Fix: link de portfolio no footer

--- src/components/Commons/Footer/index.jsx
+++ src/components/Commons/Footer/index.jsx
@@ -3,10 +3,10 @@ import React from "react";
 export default function Footer(params) {
     const Year = new Date().getFullYear();
     return (
-        <footer className="border-t border-neutro-300 border-opacity-20 flex justify-between items-center py-3 px-2 text-neutro-600">
+        <footer className="border-t border-neutro-300 border-opacity-20 flex justify-between items-center flex-col md:flex-row py-3 px-1 md:px-2 text-neutro-600">
             <p className="text-xs">© {Year} StudeNotes. Todos os direitos reservados.</p>
             <span className="text-xs flex items-center flex-col">
-                <p>Desenvolvido por <a href="https://matheussousa.vercel.app/home" className="text-azul-200">Matheus Sousa</a>.</p>
+                <p>Desenvolvido por <a href="https://matheussousa.vercel.app/home" target="_blank" className="text-azul-200 hover:underline-offset-1 hover:underline">Matheus Sousa</a>.</p>
             </span>
         </footer>
     )
