dc94cf5677febdbe82657f57519cc3c4dd5cde33
Matheus Sousa <matheussousab2014@gmail.com>
2024-03-20 16:59:02 -0300

    Add: novas opções de pesquisa

--- src/components/Commons/Search/index.jsx
+++ src/components/Commons/Search/index.jsx
@@ -9,9 +9,9 @@ export default function Search(search) {
     return (
         <div className="search-content">
             <div className="flex w-full md:w-1/2 items-center justify-center gap-1 bg-white rounded-lg">
-                <input type="text" className="input-search" placeholder={`Pesquisar ${search.type}`} value={search.nome} onChange={(event) => search.setSearchNome(event.target.value)} />
+                <input type="text" className="input-search" placeholder={`Pesquisar ${search.type}`} value={search.nome} onChange={(event) => search.setSearchNome(event.target.value)} onKeyDown={(event) => {if (event.key === 'Enter') {search.buscar();}}}/>
                 <button type="button" className="btn-filter" onClick={() => setModalFilters(!modalFilters)} title="Filtros">{modalFilters ? <X size={17} /> : <Faders size={17} />}</button>
-                <button type="submit" className="btn-search" title="Pesquisar"><MagnifyingGlass size={17} /></button>
+                <button type="button" className="btn-search" onClick={() => search.buscar()} title="Pesquisar"><MagnifyingGlass size={17} /></button>
             </div>
             {modalFilters &&
                 <div className="subsearch-content">
@@ -26,6 +26,16 @@ export default function Search(search) {
                             <span className="input-group-search">
                                 <label htmlFor="data_fim" className="label-input">Criado até</label>
                                 <input type="date" name="data_fim" id="data_fim" value={search.data_fim} onChange={(event) => search.setSearchDateFim(event.target.value)} min={search.data_inicio} className="input-search-date" />
+                            </span>
+                        }
+                        {search.setSearchStatus &&
+                            <span className="input-group-search">
+                                <label htmlFor="status" className="label-input">Status</label>
+                                <select name="status" id="status" className="input-search-date" onChange={(event) => search.setSearchStatus(event.target.value)}>
+                                    <option value="" selected={search.status === ""}>Ativos</option>
+                                    <option value="deletados" selected={search.status === "deletados"}>Inativos</option>
+                                    <option value="ambos" selected={search.status === "ambos"}>Ambos</option>
+                                </select>
                             </span>
                         }
                     </div>
--- src/pages/Admin/Disciplina/index.jsx
+++ src/pages/Admin/Disciplina/index.jsx
@@ -4,6 +4,7 @@ import Pagination from "../../../components/Commons/Pagination";
 import Search from "../../../components/Commons/Search";
 import MainHeader from "../../../components/Commons/MainHeader";
 import Loading from "../../../components/Commons/Loading";
+import { ArrowClockwise, PencilSimple, TrashSimple } from "@phosphor-icons/react";
 
 export default function DisciplinaAdminPage(params) {
     const [disciplinas, setDisciplinas] = useState([]);
@@ -11,6 +12,7 @@ export default function DisciplinaAdminPage(params) {
     const [searchNome, setSearchNome] = useState("");
     const [searchDateInicio, setSearchDateInicio] = useState("");
     const [searchDateFim, setSearchDateFim] = useState("");
+    const [searchStatus, setSearchStatus] = useState("");
 
     const [cadastrarDisciplina, setCadastrarDisciplina] = useState(false);
 
@@ -21,7 +23,8 @@ export default function DisciplinaAdminPage(params) {
                     page: page,
                     nome: searchNome,
                     data_inicio: searchDateInicio,
-                    data_fim: searchDateFim
+                    data_fim: searchDateFim,
+                    delete: searchStatus
                 }
             });
             setDisciplinas(response.data.data);
@@ -35,6 +38,8 @@ export default function DisciplinaAdminPage(params) {
         setSearchNome("");
         setSearchDateFim("");
         setSearchDateInicio("");
+        setSearchStatus("");
+        receiveDisciplinas();
     };
 
     useEffect(() => {
@@ -52,23 +57,63 @@ export default function DisciplinaAdminPage(params) {
                 text='Uma lista das disciplinas cadastradas incluindo id, nome e status.'
                 setFunction={setCadastrarDisciplina}
             />
-            <form onSubmit={receiveDisciplinas}>
-                <Search
-                    type="disciplinas"
-                    nome={searchNome}
-                    setSearchNome={setSearchNome}
-                    data_inicio={searchDateInicio}
-                    setSearchDateInicio={setSearchDateInicio}
-                    data_fim={searchDateFim}
-                    setSearchDateFim={setSearchDateFim}
-                    limpar={limparSearch}
-                />
-            </form>
-            <div className="conteudo-content pt-5">
-                {/* <div className="header-content">
-                    <p>Uma lista de todas as disciplinas cadastradas, incluindo o id e o nome.</p>
-                    <button className="btn-add">Adicionar Disciplina</button>
-                </div> */}
+            <Search
+                type="disciplinas"
+                nome={searchNome}
+                setSearchNome={setSearchNome}
+                data_inicio={searchDateInicio}
+                setSearchDateInicio={setSearchDateInicio}
+                data_fim={searchDateFim}
+                setSearchDateFim={setSearchDateFim}
+                status={searchStatus}
+                setSearchStatus={setSearchStatus}
+                limpar={limparSearch}
+                buscar={receiveDisciplinas}
+            />
+            <div className="conteudo-content">
+                <table className="w-full bg-white rounded-lg text-sm table-auto relative">
+                    <thead>
+                        <tr className="table-row-header">
+                            <th className="sticky w-[20%] rounded-tl-lg">ID</th>
+                            <th className="sticky w-[40%]">Nome</th>
+                            <th className="sticky w-[20%]">Status</th>
+                            <th className="sticky w-[20%] rounded-tr-lg">Ações</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {disciplinas.map((disciplina, i) => (
+                            <tr key={i} className="table-row-body">
+                                <td className="w-[20%] font-medium">{disciplina.id}</td>
+                                <td className="w-[40%]">{disciplina.nome}</td>
+                                {disciplina.deleted_at === null ?
+                                    <td className="w-[20%]">
+                                        <div className="active-card">
+                                            Ativo
+                                        </div>
+                                    </td>
+                                    :
+                                    <td className="w-[20%]">
+                                        <div className="inactive-card">
+                                            Inativo
+                                        </div>
+                                    </td>
+                                }
+                                <td className="w-[20%]">
+                                    {disciplina.deleted_at === null ?
+                                        <div className="content-buttons-action">
+                                            <button type="button" className="edit-action-btn" title="Editar"><PencilSimple size={20} /></button>
+                                            <button type="button" className="delete-action-btn" title="Excluir"><TrashSimple size={20} /></button>
+                                        </div>
+                                        :
+                                        <div>
+                                            <button type="button" className="restore-action-btn" title="Restaurar"><ArrowClockwise size={20} /></button>
+                                        </div>
+                                    }
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
             </div>
             <div>
                 {pagination && (
