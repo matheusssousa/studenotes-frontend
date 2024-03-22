c28137cce4a13dfb077bcfb96e07261bf0503309
Matheus Sousa <matheussousab2014@gmail.com>
2024-03-22 16:48:41 -0300

    Fix: modal delete

--- src/components/Commons/Modals/Delete/index.jsx
+++ src/components/Commons/Modals/Delete/index.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 
+import '../modal.css';
+
 export default function ModalDelete(param) {
+    console.log(param);
     return (
         <div className="modal-overlay">
             <div className="modal">
-                <p>{param.title}</p>
-                <p>Tem certeza que deseja excluir {param.item}?</p>
+                <p className="title-modal">Excluir {param.item.nome}</p>
+                <p className="text-modal">Tem certeza que deseja excluir {param.item.nome}?</p>
                 <div className="container-action-buttons">
-                    <button type="button" title="Confirmar" className="confirm-button-modal" onClick={() => param.delete()}>Confirmar</button>
+                    <button type="button" title="Confirmar" className="confirm-button-modal" onClick={() => param.delete(param.item.id)}>Confirmar</button>
                     <button type="button" title="Cancelar" className="cancel-button-modal" onClick={() => param.cancel(false)}>Cancelar</button>
                 </div>
             </div>
