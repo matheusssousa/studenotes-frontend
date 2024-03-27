15bce3f0c234f1c54b96ebef7c6ee62265aaa784
Matheus Sousa <matheussousab2014@gmail.com>
2024-03-27 17:12:35 -0300

    Add: rotas de categorias

--- src/routes/User/PrivateUserRoutes.jsx
+++ src/routes/User/PrivateUserRoutes.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import HomeUserPage from "../../pages/User/Home";
 import CategoriaUserPage from "../../pages/User/Categoria";
+import AddOrEditCategoriaUserPage from "../../pages/User/Categoria/addOrEdit.jsx";
 
 export default function PrivateRoutesUser(params) {
     return (
         <Routes>
             <Route path="*" element={<Navigate to='/home' />} />
             <Route path="/home" element={<HomeUserPage />} />
-            <Route path="/categorys" element={<CategoriaUserPage />} />
+            <Route path="/categorias" element={<CategoriaUserPage />} />
+            <Route path="/categorias/addedit/:id?" element={<AddOrEditCategoriaUserPage />} />
         </Routes>
     )
 }
