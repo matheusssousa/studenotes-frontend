beb6adca0b208211e8c2d9d8813661e4c9db6ae2
Matheus Sousa <matheussousab2014@gmail.com>
2024-04-01 10:24:45 -0300

    Fix: table e viewmode

--- src/components/Commons/Table/index.jsx
+++ src/components/Commons/Table/index.jsx
@@ -29,6 +29,7 @@ const Table = (params) => {
             { label: "ID", width: "10%" },
             { label: "Nome", width: "40%" },
             { label: "Email", width: "30%", hideOnMobile: true },
+            { label: "Email Verificado", width: "20%", hideOnMobile: true },
             { label: "Status", width: "20%", hideOnMobile: true },
             { label: "Ações", width: "10%" },
         ],
@@ -54,17 +55,19 @@ const Table = (params) => {
                     <tr key={i} className="table-row-body">
                         <td className="w-[10%]">{item.id}</td>
                         {item.cor && (<td className="w-[10%]"><div className="w-5 h-5 rounded-full" style={{ backgroundColor: `${item.cor}` }} /></td>)}
-                        {item.nome && (<td className="w-[40%]">{item.nome}</td>)}
+                        {item.nome && (<td className={`${(params.type === 'disciplinas' || params.type === 'categorias') && 'w-full'} w-[40%]`}>{item.nome}</td>)}
                         {item.name && (<td className="w-[40%]">{item.name}</td>)}
                         {item.email && (<td className="w-[30%]">{item.email}</td>)}
-                        {item.email_verified_at &&
-                            (item.email_verified_at === null ?
+                        {!item.email_verified_at ?
+                            (item.email_verified_at === null &&
                                 <td className="w-[20%] hidden md:table-cell">
                                     <div className="inactive-email-card">
                                         Não verificado
                                     </div>
                                 </td>
-                                :
+                            )
+                            :
+                            (item.email_verified_at &&
                                 <td className="w-[20%] hidden md:table-cell">
                                     <div className="active-email-card">
                                         Verificado
@@ -81,14 +84,14 @@ const Table = (params) => {
                             :
                             <td className="w-[10%]">
                                 <div className="inactive-card">
-                                    Inativo
+                                    Excluído
                                 </div>
                             </td>
                         }
                         <td className="w-[20%]">
                             {item.deleted_at === null ?
                                 <div className="content-buttons-action">
-                                    <Link to={`${params.admin ? `/admin`:``}/${params.type}/addedit/${item.id}`} className="edit-action-btn" title="Editar"><PencilSimple size={20} /></Link>
+                                    <Link to={`${params.admin ? `/admin` : ``}/${params.type}/addedit/${item.id}`} className="edit-action-btn" title="Editar"><PencilSimple size={20} /></Link>
                                     <button type="button" className="delete-action-btn" title="Excluir" onClick={() => params.delete(item.id)}><TrashSimple size={20} /></button>
                                 </div>
                                 :
