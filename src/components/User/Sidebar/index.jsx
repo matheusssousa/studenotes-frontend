f0f84c9c87974e0846d925fabf7c21063dd69574
Matheus Sousa <matheussousab2014@gmail.com>
2024-04-01 10:23:23 -0300

    Fix: sidebar para usuário

--- src/components/User/Sidebar/index.jsx
+++ src/components/User/Sidebar/index.jsx
@@ -1,19 +1,24 @@
 import React, { useState } from "react";
 import { useTheme } from "../../../context/Theme/ThemeContext";
 import { useAuth } from "../../../context/Authenticate/AuthContext";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { House, ChatsTeardrop, TextAlignLeft, MoonStars, SunHorizon, BookmarksSimple, Note } from "@phosphor-icons/react";
 // import BigHeads from "../../../hooks/Avatars";
 import Logo from "../../../assets/Logo";
 
 export default function SidebarUser(params) {
-    const [sidebarOpen, setSidebarOpen] = useState(false);
     const { theme, setTheme } = useTheme();
     const { user, LogoutUser } = useAuth();
+    const [sidebarOpen, setSidebarOpen] = useState(false);
+    const [userOptions, setUserOptions] = useState(false);
+
+    const firstName = user.name.split(' ')[0];
+    const lastName = user.name.split(' ')[1];
+    const letters = `${firstName.charAt(0)}${lastName ? lastName.charAt(0) : ''}`;
 
     const links = [
         { name: "Página Inicial", link: "/home", icon: House },
-        { name: "Anotacoes", link: "/notes", icon: Note },
+        { name: "Anotacoes", link: "/anotacoes", icon: Note },
         { name: "Categorias", link: "/categorias", icon: BookmarksSimple },
         { name: "Comunidade", link: "/comunidade", icon: ChatsTeardrop },
     ];
@@ -24,11 +29,35 @@ export default function SidebarUser(params) {
             await LogoutUser();
         } catch (error) { }
     }
+    const handleNavLinkClick = () => {
+        if (sidebarOpen) {
+            setSidebarOpen(false);
+        }
+    };
+
+    const handleNavLinkUserClick = () => {
+        if (userOptions) {
+            setUserOptions(!userOptions)
+            setSidebarOpen(!sidebarOpen)
+        } else {
+            setSidebarOpen(!sidebarOpen)
+        }
+    }
+
+    const handleNavLinkUserOptionClick = () => {
+        if (sidebarOpen) {
+            setSidebarOpen(false)
+            setUserOptions(!userOptions)
+        } else {
+            setUserOptions(!userOptions)
+        }
+    }
+    
     return (
         <nav className={`sidebar-admin ${sidebarOpen ? 'open' : 'close'}`}>
             <div className="row-cel-pc">
-                <button className={`btn-open-sidebar ${sidebarOpen && 'text-azul-200'}`} onClick={() => setSidebarOpen(!sidebarOpen)}><TextAlignLeft size={23} /></button>
-                <Logo cor='azul' tamanho='sidebar' />
+                <button className={`btn-open-sidebar ${sidebarOpen && 'text-azul-200'}`} onClick={handleNavLinkUserClick}><TextAlignLeft size={23} /></button>
+                <Link to='/home'><Logo cor='azul' tamanho='sidebar' /></Link>
                 <div className="menu" aria-hidden='true'>
                     {links.map((links, i) => (
                         <NavLink
@@ -49,7 +78,7 @@ export default function SidebarUser(params) {
                         <MoonStars size={23} onClick={() => setTheme("dark")} className="buttonmoon" /> :
                         <SunHorizon size={23} onClick={() => setTheme("light")} className="buttonsun" />
                     }
-                    <button className="w-10 h-10 hover:drop-shadow-lg duration-300 bg-azul-200 rounded-full"></button>
+                    <button className={`letters-user${userOptions ?'-active':''}`} onClick={handleNavLinkUserOptionClick}>{letters}</button>
                 </div>
             </div>
             <div className="menu-cel" aria-hidden='true'>
@@ -57,6 +86,7 @@ export default function SidebarUser(params) {
                     <NavLink
                         key={i}
                         to={links.link}
+                        onClick={handleNavLinkClick}
                         className={({ isActive, isPending }) =>
                             isPending ? "link-pending group" : isActive ? "link-active group" : "link-pending group"
                         }>
@@ -66,6 +96,12 @@ export default function SidebarUser(params) {
                         </span>
                     </NavLink>
                 ))}
+            </div>
+            <div className={`user-menu-options ${!userOptions && 'hidden'}`}>
+                <Link to='/conta' className="text-center hover:drop-shadow duration-300">Conta</Link>
+                <button type="button" onClick={Logout} className="hover:drop-shadow duration-300">Sair</button>
+                <div className="w-full h-px bg-neutro-300 bg-opacity-20 my-2"/>
+                <small className="text-center">{user.name}</small>
             </div>
         </nav>
     )
