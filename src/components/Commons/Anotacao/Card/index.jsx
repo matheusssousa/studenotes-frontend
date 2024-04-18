0e88bd32cb7c4093773ddf0bcba0fcd0cb85a13a
Matheus Sousa <matheussousab2014@gmail.com>
2024-04-18 17:59:03 -0300

    Fix: card de anotacao, estilo

--- src/components/Commons/Anotacao/Card/index.jsx
+++ src/components/Commons/Anotacao/Card/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Files, File } from "@phosphor-icons/react";
+import moment from "moment";
 import darkColor from "../../../../hooks/DarkColor";
 
 import './style.css';
@@ -14,29 +15,39 @@ export default function AnotacaoCard(params) {
     return (
         <Link to={`/anotacoes/view/${params.item.id}`} className="card-anotacao">
             <div className="content-header-card-anotacao">
-                <span>
-                    <p className="text-base font-medium">{params.item.nome}</p>
-                    {/* <small className="text-neutro-300">{params.item.disciplina.nome}</small> */}
+                <div className="w-[15%]" />
+                <span className=" w-[80%] md:w-[70%] flex items-center justify-center flex-col">
+                    <p className="text-base font-medium text-center">{params.item.nome}</p>
+                    <div className="flex gap-3 -translate-y-1">
+                        {params.item.disciplina && <small className="text-neutro-300">{params.item.disciplina.nome}</small>}
+                        {params.item.data_prazo && <small className="text-neutro-300">{moment(params.item.data_prazo).format('DD-MM-YYYY')}</small>}
+                    </div>
                 </span>
-                <span className="flex gap-2 flex-wrap">
+                <span className="w-[15%] flex gap-1 flex-wrap items-start justify-center">
                     {params.item.categorias.map((categoria, i) => (
-                        <div key={i} style={{ backgroundColor: `${categoria.cor}`, borderColor: darkenColor(categoria.cor, 40) }} className="w-7 h-3 rounded-full border" />
+                        <div key={i} style={{ backgroundColor: `${categoria.cor}`, borderColor: darkenColor(categoria.cor, 40) }} className="w-5 h-2 rounded-full border" />
                     ))}
                 </span>
             </div>
-            <div className="content-texto-card-anotacao">
-                {params.item.texto > 150 ? params.item.texto.substring(0, 100) + '...' : params.item.texto}
+            <div className="content-texto-card-anotacao break-all">
+                {params.item.texto.length > 400 ? params.item.texto.substring(0, 400) + '...' : params.item.texto}
+            </div>
+            <div className="content-texto-card-anotacao-mobile">
+                {params.item.texto.length > 70 ? params.item.texto.substring(0, 70) + '...' : params.item.texto}
             </div>
             <div className="content-options-card-anotacao">
                 <div>
-                    {params.item.arquivos > 1 ?
-                        <span className="flex gap-1 text-neutro-300 items-center font-semibold rounded-md"><Files size={12} /> <small>Arquivos</small></span>
-                        :
-                        <span className="flex gap-1 text-neutro-300 items-center font-semibold rounded-md"><File size={12} /> <small>Arquivo</small></span>
-                    }
-                </div>
-                <div>
-
+                    {params.item.arquivos.length > 0 ? (
+                        params.item.arquivos.length > 1 ? (
+                            <span className="flex gap-1 text-neutro-300 items-center font-semibold rounded-md">
+                                <Files size={12} /> <small>Arquivos</small>
+                            </span>
+                        ) : (
+                            <span className="flex gap-1 text-neutro-300 items-center font-semibold rounded-md">
+                                <File size={12} /> <small>Arquivo</small>
+                            </span>
+                        )
+                    ) : null}
                 </div>
             </div>
         </Link>
