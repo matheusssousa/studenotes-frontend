b9b8f24ff21e2a7e428c4ecfbcd212ed89d5d9f5
Matheus Sousa <matheussousab2014@gmail.com>
2024-07-10 12:12:44 -0300

    add: estrutura para opções de ia

--- src/components/Commons/OptionsGPT/index.jsx
+++ src/components/Commons/OptionsGPT/index.jsx
@@ -1,16 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { motion } from "framer-motion";
 
 import "./style.css";
 
-export default function OptionsGPT({ options, setOptions }) {
+export default function OptionsGPT({ setOptions }) {
+    const ref = useRef(null);
+    const [option, setOption] = useState(null);
+
+    useEffect(() => {
+        function handleClickOutside(event) {
+            if (ref.current && !ref.current.contains(event.target)) {
+                setOptions(false);
+            }
+        }
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [setOptions]);
+
     return (
         <motion.div
-            initial={{ scale: 0.5 }}
-            animate={{ scale: 1 }} 
-            exit={{ scale: 0.5 }}
+            ref={ref}
+            initial={{ width: 0, height: 0, opacity: 0 }}
+            animate={{ width: '40%', height: 250, opacity: 1, transition: { duration: 0.2 }}}
+            exit={{ width: 0, height: 0, opacity: 0, transition: { duration: 0.3 } }}
             className="options-gpt-container">
-
+                
         </motion.div>
     );
 }
