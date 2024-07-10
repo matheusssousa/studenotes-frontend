a8aefe9616c433db4f7dfb34064fc1b48eda07a6
Matheus Sousa <matheussousab2014@gmail.com>
2024-07-10 12:05:48 -0300

    fix: otimizado header anotacao

--- src/components/Commons/Anotacao/Header/index.jsx
+++ src/components/Commons/Anotacao/Header/index.jsx
@@ -1,86 +1,64 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { CaretLeft, ClockClockwise, PencilSimple, ShareFat, TrashSimple } from "@phosphor-icons/react";
 import moment from "moment";
-import adjustColor from "../../../../hooks/AdjustColor";
+import { useAuth } from "../../../../context/Authenticate/AuthContext";
+import "./style.css";
+import ViewCategorias from "../../Categoria/ViewCategorias";
+import BackButton from "../../Buttons/Back";
+import DeleteMinimalist from "../../Buttons/Delete/DeleteMinimalist";
+import EditMinimalist from "../../Buttons/Edit/EditMinimalist";
+import ShareMinimalist from "../../Buttons/Share/ShareMinimalist";
+import LikeButton from "../../Buttons/Like";
+import RestoreMinimalist from "../../Buttons/Restore/RestoreMinimalist";
 
-export default function HeaderAnotacao(params) {
-    const darkenColor = (hex, percent) => {
-        const dark = adjustColor(hex, percent);
-        return dark;
-    };
+const ActionButtons = ({ onDelete, id }) => (
+    <>
+        <EditMinimalist edit={`/anotacoes/addedit/${id}`} />
+        {onDelete && (
+            <DeleteMinimalist onClick={() => onDelete(id)} />
+        )}
+    </>
+);
+
+export default function HeaderAnotacao({ params, voltar, restore, onDelete }) {
+    const { user, admin } = useAuth();
+    const { id, nome, disciplina, data_prazo, categorias, username } = params;
 
     return (
-        <div className="w-full pt-2 flex items-center md:items-start justify-between">
+        <div className="content-header-anotacao">
             <div className="w-[5%] flex justify-start">
-                {params.voltar &&
-                    <Link
-                        to={params.voltar}
-                        className="sm:absolute sm:left-14 bg-azul-200 py-2 px-2 sm:px-0 rounded-md sm:rounded-l-none sm:rounded-r-md hover:drop-shadow-md duration-200 ease-in hover:text-neutro-100"
-                        title="Voltar">
-                        <CaretLeft size={15} />
-                    </Link>
-                }
-                <form onSubmit={params.setComunidade}>
-                    <button type="submit" className={`toogle-label ${params.comunidade === 1 ? 'bg-azul-200' : 'bg-neutro-250'}`}>
-                        <p className={`toogle-button ${params.comunidade === 1 ? 'text-neutro-400' : 'text-neutro-300'}`}>
-                            <ShareFat size={16} />
-                        </p>
-                    </button>
-                </form>
+                {voltar && <BackButton voltar={voltar} />}
+                {username && username.id === user?.id && (
+                    <div className="py-2">
+                        <ShareMinimalist id={id} anotacao_comunidade={params.comunidade} />
+                    </div>
+                )}
             </div>
             <span className="flex justify-center flex-col items-center">
-                <p className="font-medium dark:text-neutro-100">{params.title}</p>
-                {(params.disciplina || params.data_prazo) &&
+                <p className="font-medium dark:text-neutro-100">{nome}</p>
+                {(disciplina || data_prazo || username) && (
                     <span className="flex gap-2">
-                        {params.disciplina && <p className="text-xs text-neutro-300">{params.disciplina}</p>}
-                        {params.data && <p className="text-xs text-neutro-300">{moment(params.data).format('DD-MM-YYYY')}</p>}
+                        {disciplina && <p className="text-xs text-neutro-300">{disciplina.nome}</p>}
+                        {data_prazo && <p className="text-xs text-neutro-300">{moment(data_prazo).format('DD-MM-YYYY')}</p>}
+                        {username && voltar === '/comunidade' && <p className="text-xs text-neutro-300">{username.name}</p>}
                     </span>
-                }
-                {params.categorias &&
-                    <span className="flex gap-1 mt-2">
-                        {params.categorias.map((categoria, i) => (
-                            <span key={i} className="px-1 text-xs font-semibold flex gap-2 border-2 rounded-md"
-                                style={{
-                                    backgroundColor: `${categoria.cor}`,
-                                    color: darkenColor(categoria.cor, 40),
-                                    borderColor: darkenColor(categoria.cor, 40)
-                                }}>
-                                {categoria.nome}
-                            </span>
-                        ))}
-                    </span>
-                }
+                )}
+                {categorias && (
+                    <ViewCategorias categorias={categorias} />
+                )}
             </span>
-            <div className="w-[5%] flex justify-end">
-                {params.restore ?
-                    <button
-                        onClick={() => params.restore(params.anotacao)}
-                        className="flex gap-1 rounded-lg bg-verde-100 px-2 py-1 text-sm items-center justify-center hover:bg-verde-200 hover:text-neutro-100 duration-200"
-                        title="Restaurar">
-                        <ClockClockwise size={15} />
-                    </button>
-                    :
+            <div className="w-[5%] flex justify-end py-2">
+                {username && username.id == user?.id ? (
                     <div className="content-buttons-action flex-col md:flex-row">
-                        {params.edit &&
-                            <Link
-                                to={params.edit}
-                                className="flex gap-1 rounded-lg bg-azul-200 px-2 py-1 text-sm items-center justify-center hover:bg-azul-300 hover:text-neutro-100 duration-200"
-                                title="Editar">
-                                <PencilSimple size={15} />
-                            </Link>
-                        }
-                        {params.delete &&
-                            <button
-                                onClick={() => params.delete(true)}
-                                className="flex gap-1 rounded-lg bg-vermelho-200 px-2 py-1 text-sm items-center justify-center hover:bg-vermelho-300 hover:text-neutro-100 duration-200"
-                                title="Excluir">
-                                <TrashSimple size={15} />
-                            </button>
-                        }
+                        {restore ? (
+                            <RestoreMinimalist onClick={() => restore(id)} />
+                        ) : (
+                            <ActionButtons onDelete={onDelete} id={id} />
+                        )}
                     </div>
-                }
+                ) : (
+                    <LikeButton id={id} liked={params.curtida} />
+                )}
             </div>
         </div>
-    )
+    );
 }
