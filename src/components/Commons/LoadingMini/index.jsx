4cea8dad047b10e07af8cc40c1043365fa83a724
Matheus Sousa <matheussousab2014@gmail.com>
2024-07-10 12:03:13 -0300

    fix: novo estilo para loading

--- src/components/Commons/LoadingMini/index.jsx
+++ src/components/Commons/LoadingMini/index.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 import "./style.css";
 
-export default function Loading() {
+export default function LoadingMini(params) {
     return (
-        <div className="loading-content">
+        <div className="w-full flex items-center justify-center">
             <svg className="spinner" viewBox="0 0 50 50">
                 <circle className="path" cx="25" cy="25" r="20" fill="none" strokeWidth="4"></circle>
             </svg>
         </div>
-    )
+    );
 }
